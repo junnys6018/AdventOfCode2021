eb1a0ce2f43660d87d0d7125c4070500d7b7d0ea
junnys6018 <junkanglim1@gmail.com>
2021-12-24 17:40:34 +0800

    cli

--- main.go
+++ main.go
@@ -3,9 +3,24 @@ package main
 import (
 	"advent/day01"
 	"advent/day02"
+
+	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
-	day01.Answer()
-	day02.Answer()
+	day, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	switch day {
+	case 1:
+		day01.Answer()
+	case 2:
+		day02.Answer()
+	default:
+		fmt.Fprintf(os.Stderr, "Invalid day: %v\n", day)
+	}
 }
